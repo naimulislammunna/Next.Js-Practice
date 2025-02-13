f42d0d7cc3913d71ed879bedbefb5cc32621bd69
naimulislammunna <naimulislammunna742@gmail.com>
2025-02-13 23:22:25 +0600

    home page set

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Next running</h1>
+    <div className="place-content-center place-items-center h-screen">
+        <div>
+        <h1 className="text-6xl font-bold text-gray-950 text-center">Practice Our Next Project</h1>
+        <p className="text-2xl font-bold text-gray-950 text-center my-2">The React Framework for the Web</p>
+        </div>
+    </div>
     </>
   );
 }
